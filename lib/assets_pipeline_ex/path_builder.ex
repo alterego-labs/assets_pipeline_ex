e16e7eb0f9f8adf21f81de789e7e6767da15cd55
Sergey Gernyak <sergeg1990@gmail.com>
2014-10-31 17:12:04 +0200

    FEAT: PathBuilder basic logic

--- lib/assets_pipeline_ex/path_builder.ex
+++ lib/assets_pipeline_ex/path_builder.ex
@@ -1,5 +1,19 @@
 defmodule AssetsPipelineEx.PathBuilder do
   def path_for(filename, :js = asset_type) do
-    
+    filename
+    |> to_string
+    |> apply_path_and_ext(asset_type)
+  end
+
+  defp apply_path_and_ext(filename, asset_type) do
+    base_path(asset_type) <> filename <> extension(asset_type)
+  end
+
+  defp base_path(:js) do
+    "priv/assets/js/"
+  end
+
+  defp extension(:js) do
+    ".js.coffee"
   end
 end
