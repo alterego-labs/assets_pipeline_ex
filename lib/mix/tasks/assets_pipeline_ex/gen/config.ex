219be84696a3fb4bec802f1f3f3e1b38b63e13e3
Sergey Gernyak <sergeg1990@gmail.com>
2014-10-31 13:14:48 +0200

    FEAT: config generator

---
+++ lib/mix/tasks/assets_pipeline_ex/gen/config.ex
@@ -0,0 +1,23 @@
+defmodule Mix.Tasks.AssetsPipelineEx.Gen.Config do
+  use Mix.Task
+  import Mix.Generator
+
+  @shortdoc "Creates AssetsPipelineEx config file"
+
+  @moduledoc """
+  Creates AssetsPipelineEx config file
+  """
+
+  def run(args) do
+    create_config_file
+  end
+
+  defp create_config_file do
+    create_file "config/assets_pipeline_ex.exs", config_template
+  end
+
+  embed_template :config, ~S"""
+  #Specify list of compiling assets
+  config :assets_pipeline_ex, :javascripts, []
+  """
+end
