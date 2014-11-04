7f559fff3c25b784edab6ff08ff3ca3380e3d76a
Sergey Gernyak <sergeg1990@gmail.com>
2014-11-04 15:20:08 +0200

    Provide package description

--- mix.exs
+++ mix.exs
@@ -5,7 +5,10 @@ defmodule AssetsPipelineEx.Mixfile do
     [app: :assets_pipeline_ex,
      version: "0.0.1",
      elixir: "~> 1.0.0",
-     deps: deps]
+     description: "The asset pipeline provides a framework to pre-process JavaScripts
+assets in other language such as CoffeeScript",
+     deps: deps,
+     package: package]
   end
 
   # Configuration for the OTP application
@@ -19,5 +22,12 @@ defmodule AssetsPipelineEx.Mixfile do
     [
       { :execjs, github: "devinus/execjs", tag: "1.1.1" }
     ]
+  end
+
+  defp package do
+    [ files: ["lib", "config", "priv", "test", "mix.exs", "README.md"],
+      contributors: ["Sergey Gernyak"],
+      licenses: ["MIT"],
+      links: %{ "GitHub": "https://github.com/alterego-labs/assets_pipeline_ex" } ]
   end
 end
